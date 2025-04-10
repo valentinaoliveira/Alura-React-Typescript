be9f87dc3b63c3a4d3d9522bc07e4662ad99f334
valentinaoliveira <voli.leite05@outlook.com>
2025-04-10 11:26:27 -0300

    Adicionando arquivo que havia sido apagado

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Botao from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+          <Botao />
+    </div>
+  );
+}
+
+export default App;
